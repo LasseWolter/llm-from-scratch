0b69f0361c447f2814dc5cdddbd8af4f6336f886
LasseWolter <wolter.lasse@gmail.com>
2025-04-14 20:12:39 +0100

    feat: Create tokenised training and validation data

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
-text: str
+import torch
+torch.device('cpu')
+
+corpus: str
 
 with open("input.txt", "r") as file:
-    text = file.read()
+    corpus = file.read()
 
-vocab: list[str] = sorted(list(set(text)))
+vocab: list[str] = sorted(list(set(corpus)))
 
 print(f"vocab length: {len(vocab)}")
 print("vocab:")
@@ -32,4 +35,17 @@ print(f"encoded:{encoded}")
 decoded = decode(encoded)
 print(f"decoded:{decoded}")
 
+data = torch.tensor(encode(corpus), dtype=torch.long)
 
+print(data[:1000])
+
+n = int(len(data) * 0.9)
+train_data = data[:n]
+test_data = data[n:]
+
+assert (
+    train_data.shape[0] + test_data.shape[0] == data.shape[0]
+), "the number of elements in training and validation set doesn't add up to full number of elements"
+
+print(f"Number of train elements: {train_data.shape[0]}")
+print(f"Number of validation elements: { train_data.shape[0] }")
